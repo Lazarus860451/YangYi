f57527f7eaa520bbc26a0f2d0d455084f686afc4
Lazarus860451 <iignacius@outlook.com>
2024-09-22 15:40:01 +0300

    1.cpp
    Задание №06

--- 1.cpp
+++ 1.cpp
@@ -6,9 +6,9 @@ int main()
     int n1,n2,s;
     cout << "Enter two integers: ";
     cin >> n1 >>n2;
-    s = n1 + n2;
+    s = n1 - n2;
     cout << n1 << " is the first integer you entered";
     cout << n2 << " is the second integer you entered";
-    cout << s << " is the sum of the two integers you entered";
+    cout << s << " is the difference of the two integers you entered";
     return 0;
 }
