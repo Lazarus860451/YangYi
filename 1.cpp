b94a2dd9b9298c92de0cf1dc5f60eff1fa4dec10
Yyang <iignacius@outlook.com>
2024-09-22 15:28:59 +0300

    two_intergers

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,12 @@
 #include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1,n2,s;
+    cout << "Enter two integers: ";
+    cin >> n1 >>n2;
+    cout << n1 << " is the first integer you entered";
+    cout << n2 << " is the second integer you entered";
+    return 0;
+}
